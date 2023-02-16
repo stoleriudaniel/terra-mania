08e77ed6c41f571868d0123a7dc36abd67ce84e3
stoleriudaniel <stoleriu.daniel47@gmail.com>
2023-02-16 21:55:29 +0200

    Created tree algorithm to find country pixels fast

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 import HandTrackingModule
 import HandTrackingModule as HTM
 import numpy as np
@@ -9,11 +12,35 @@ from fontTools.ttLib import TTFont
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
-def writeCountryPixelsInFile2(countryName, mousex, mousey):
+
+def initTreePixels():
+    directories = [x[0] for x in os.walk('countries')]
+    print(directories)
+    countries = []
+    for index in range(0, len(directories)):
+        country = directories[index].split("\\")
+        if len(country) > 1:
+            path = Path(f"countries\\{country[1]}\\pixels.txt")
+            if not path.is_file():
+                continue
+            pixelsFile = open(f"countries\\{country[1]}\\pixels.txt", "r")
+            rows = pixelsFile.readlines()
+            print(country[1])
+            for row in rows:
+                result = row.split()
+                xCord = result[0]
+                yCord = result[1]
+                filename = int(xCord) // 100 * 100
+                file = open(f"tree\\pixels\\{filename}.txt", "a")
+                file.write(f"{xCord} {yCord} {country[1]}\n")
+    print("Finish")
+
+
+def writeCountryPixelsInFile(countryName, mousex, mousey):
     file = open(f"countries\\{countryName}\\pixels.txt", "w")
-    margin = (0, 0, 0) # black
-    uncolored = (0, 51, 153) # blue
-    newColor = (238, 224, 29) # yellow
+    margin = (0, 0, 0)  # black
+    uncolored = (0, 51, 153)  # blue
+    newColor = (238, 224, 29)  # yellow
     finished = False
     uncoloredPixelsList = []
     print(mousex, mousey)
@@ -52,288 +79,35 @@ def writeCountryPixelsInFile2(countryName, mousex, mousey):
             uncoloredPixelsList.append((currentPixel[0] - 1, currentPixel[1]))
         uncoloredPixelsList = set(uncoloredPixelsList)
         uncoloredPixelsList = list(uncoloredPixelsList)
-    # while not finished:
-
-
-
-    # findMoreCountryPixels(file, window, mousex, mousey)
-    # 
-    # total = getUncoloredPixelsNeighboursFromSameCountry(window, mousex, mousey)
-    # 
-    # while total != ():
-    #     totalList = list(total)
-    #     length = len(totalList)
-    #     xPixel = totalList[length // 2][0]
-    #     yPixel = totalList[length // 2][1]
-    #     findMoreCountryPixels(file, window, xPixel, yPixel)
-    #     news = getUncoloredPixelsNeighboursFromSameCountry(window, xPixel, yPixel)
-    #     for xPixel, yPixel in totalList:
-    #         c = window.get_at((xPixel, yPixel))
-    #         color = (c[0], c[1], c[2])
-    #         if color == yellow:
-    #             totalList.remove((xPixel, yPixel))
-    #     totalNews = list(news)
-    #     totalList += totalNews
-    #     total = tuple(totalList)
     print("Finish")
     file.close()
 
-def drawCountry(country, rgb):
-    file = open(f"countries\\{country}\\pixels.txt", "r")
-    pixels = file.readlines()
-    for pixel in pixels:
+
+def drawCountry(mouseX, mouseY, initRgb, newRgb):
+    c = window.get_at((mouseX, mouseY))
+    print((c[0], c[1], c[2]) != initRgb, c)
+    if (c[0], c[1], c[2]) != initRgb:
+        return
+    country = "Italy"
+    treeFilename = mouseX // 100 * 100
+    treeFile = open(f"tree\\pixels\\{treeFilename}.txt", "r")
+    rows = treeFile.readlines()
+    for row in rows:
+        result = row.split()
+        xCord = result[0]
+        yCord = result[1]
+        if int(xCord) == mouseX and int(yCord) == mouseY:
+            country = result[2]
+    print(country)
+    countryPixelsFile = open(f"countries\\{country}\\pixels.txt", "r")
+    countryPixels = countryPixelsFile.readlines()
+    for pixel in countryPixels:
         p = pixel.split()
         x = int(p[0])
         y = int(p[1])
-        window.set_at((x, y), rgb)
+        window.set_at((x, y), newRgb)
         print(pixel)
     print("ok")
-
-
-def findMoreCountryPixels(file, window, xPos, yPos):
-    yellow = (238, 224, 29)
-    red = (255, 255, 255)  # black
-    white = (0, 0, 0)  # blue
-
-    rightPos = xPos
-    if rightPos < 0 or rightPos > SCREEN_WIDTH:
-        return
-    if yPos < 0 or yPos > SCREEN_HEIGHT:
-        return
-    c = window.get_at((rightPos + 1, yPos))
-    colorNextRightYPos = (c[0], c[1], c[2])
-    c = window.get_at((rightPos, yPos))
-    colorRightYPos = (c[0], c[1], c[2])
-    while colorRightYPos != red and colorNextRightYPos != yellow:
-        up = yPos
-        c = window.get_at((rightPos, up))
-        colorRightUp = (c[0], c[1], c[2])
-        while colorRightUp != red:
-            window.set_at((rightPos, up), yellow)
-            file.write(f"{rightPos} {up}\n")
-            up = up + 1
-            if up > SCREEN_HEIGHT:
-                break
-            print((rightPos, up))
-            c = window.get_at((rightPos, up))
-            colorRightUp = (c[0], c[1], c[2])
-        down = yPos
-        c = window.get_at((rightPos, down))
-        colorRightDown = (c[0], c[1], c[2])
-        while colorRightDown != red:
-            window.set_at((rightPos, down), yellow)
-            file.write(f"{rightPos} {down}\n")
-            down = down - 1
-            if down < 0:
-                break
-            c = window.get_at((rightPos, down))
-            colorRightDown = (c[0], c[1], c[2])
-        rightPos = rightPos + 1
-        if rightPos > SCREEN_WIDTH:
-            break
-        c = window.get_at((rightPos, yPos))
-        colorRightYPos = (c[0], c[1], c[2])
-
-    leftPos = xPos
-    c = window.get_at((leftPos - 1, yPos))
-    colorNextLeftYPos = (c[0], c[1], c[2])
-    c = window.get_at((leftPos, yPos))
-    colorLeftYPos = (c[0], c[1], c[2])
-    while colorLeftYPos != red and colorNextLeftYPos != yellow:
-        up = yPos
-        c = window.get_at((leftPos, up))
-        colorLeftUp = (c[0], c[1], c[2])
-        while colorLeftUp != red:
-            window.set_at((leftPos, up), yellow)
-            file.write(f"{leftPos} {up}\n")
-            up = up + 1
-            if up > SCREEN_HEIGHT:
-                break
-            c = window.get_at((leftPos, up))
-            colorLeftUp = (c[0], c[1], c[2])
-        down = yPos
-        c = window.get_at((leftPos, down))
-        colorLeftDown = (c[0], c[1], c[2])
-        while colorLeftDown != red:
-            window.set_at((leftPos, down), yellow)
-            file.write(f"{leftPos} {down}\n")
-            down = down - 1
-            if down < 0:
-                break
-            c = window.get_at((leftPos, down))
-            colorLeftDown = (c[0], c[1], c[2])
-        leftPos = leftPos - 1
-        if leftPos < 0:
-            break
-        c = window.get_at((leftPos, yPos))
-        colorLeftYPos = (c[0], c[1], c[2])
-
-
-def getUncoloredPixelsNeighboursFromSameCountry(window, xPos, yPos):
-    yellow = (238, 224, 29)
-    red = (255, 255, 255) #white
-    white = (0, 51, 153)  # blue // culoarea initiala
-    uncoloredPixels = ()
-
-    rightPos = xPos
-    if rightPos < 0 or rightPos > SCREEN_WIDTH:
-        return uncoloredPixels
-    if yPos < 0 or yPos > SCREEN_HEIGHT:
-        return uncoloredPixels
-    c = window.get_at((rightPos, yPos))
-    colorRightYPos = (c[0], c[1], c[2])
-
-    while colorRightYPos != red:
-        up = yPos
-        c = window.get_at((rightPos, up))
-        colorRightUp = (c[0], c[1], c[2])
-        while colorRightUp != red:
-            # verify left neighbour
-            if rightPos - 1 < 0:
-                break
-            c = window.get_at((rightPos - 1, up))
-            colorLeftNeighbour = (c[0], c[1], c[2])
-            if colorLeftNeighbour == white:
-                uncoloredPixels += ((rightPos - 1, up),)
-
-            # verify right neighbour
-            if rightPos + 1 > SCREEN_WIDTH:
-                break
-            c = window.get_at((rightPos + 1, up))
-            colorRightNeighbour = (c[0], c[1], c[2])
-            if colorRightNeighbour == white:
-                uncoloredPixels += ((rightPos + 1, up),)
-
-            up = up + 1
-            if up > SCREEN_HEIGHT:
-                break
-            c = window.get_at((rightPos, up))
-            colorRightUp = (c[0], c[1], c[2])
-
-        down = yPos
-        c = window.get_at((rightPos, down))
-        colorRightDown = (c[0], c[1], c[2])
-
-        while colorRightDown != red:
-            # verify left neighbour
-            if rightPos - 1 < 0:
-                break
-            c = window.get_at((rightPos - 1, down))
-            colorLeftNeighbour = (c[0], c[1], c[2])
-            if colorLeftNeighbour == white:
-                uncoloredPixels += ((rightPos - 1, down),)
-
-            # verify right neighbour
-            if rightPos + 1 > SCREEN_WIDTH:
-                break
-            c = window.get_at((rightPos + 1, down))
-            colorRightNeighbour = (c[0], c[1], c[2])
-            if colorRightNeighbour == white:
-                uncoloredPixels += ((rightPos + 1, down),)
-            down = down - 1
-            if down < 0:
-                break
-            c = window.get_at((rightPos, down))
-            colorRightDown = (c[0], c[1], c[2])
-
-        rightPos = rightPos + 1
-        if rightPos > SCREEN_WIDTH:
-            break
-        c = window.get_at((rightPos, yPos))
-        colorRightYPos = (c[0], c[1], c[2])
-
-    #####
-    #####
-
-    leftPos = xPos
-    c = window.get_at((leftPos, yPos))
-    colorLeftYPos = (c[0], c[1], c[2])
-    while colorLeftYPos != red:
-        up = yPos
-        c = window.get_at((leftPos, up))
-        colorLeftUp = (c[0], c[1], c[2])
-        while colorLeftUp != red:
-            # verify left neighbour
-            if leftPos - 1 < 0:
-                break
-            c = window.get_at((leftPos - 1, up))
-            colorLeftNeighbour = (c[0], c[1], c[2])
-            if colorLeftNeighbour == white:
-                uncoloredPixels += ((leftPos - 1, up),)
-
-            # verify right neighbour
-            c = window.get_at((leftPos + 1, up))
-            if leftPos + 1 > SCREEN_WIDTH:
-                break
-            colorRightNeighbour = (c[0], c[1], c[2])
-            if colorRightNeighbour == white:
-                uncoloredPixels += ((leftPos + 1, up),)
-
-            up = up + 1
-            if up > SCREEN_HEIGHT:
-                break
-            c = window.get_at((leftPos, up))
-            colorLeftUp = (c[0], c[1], c[2])
-
-        down = yPos
-        c = window.get_at((leftPos, down))
-        colorLeftDown = (c[0], c[1], c[2])
-
-        while colorLeftDown != red:
-            # verify left neighbour
-            if leftPos - 1 < 0:
-                break
-            c = window.get_at((leftPos - 1, down))
-            colorLeftNeighbour = (c[0], c[1], c[2])
-            if colorLeftNeighbour == white:
-                uncoloredPixels += ((leftPos - 1, down),)
-
-            # verify right neighbour
-            if leftPos + 1 > SCREEN_WIDTH:
-                break
-            c = window.get_at((leftPos + 1, down))
-            colorRightNeighbour = (c[0], c[1], c[2])
-            if colorRightNeighbour == white:
-                uncoloredPixels += ((leftPos + 1, down),)
-
-            down = down - 1
-            if down < 0:
-                break
-            c = window.get_at((leftPos, down))
-            colorLeftDown = (c[0], c[1], c[2])
-
-        leftPos = leftPos - 1
-        if leftPos < 0:
-            break
-        c = window.get_at((leftPos, yPos))
-        colorLeftYPos = (c[0], c[1], c[2])
-    return uncoloredPixels
-
-
-def writeCountryPixelsInFile(countryName, mousex, mousey):
-    file = open(f"countries\\{countryName}\\pixels.txt", "w")
-
-    findMoreCountryPixels(file, window, mousex, mousey)
-
-    total = getUncoloredPixelsNeighboursFromSameCountry(window, mousex, mousey)
-
-    while total != ():
-        totalList = list(total)
-        length = len(totalList)
-        xPixel = totalList[length // 2][0]
-        yPixel = totalList[length // 2][1]
-        findMoreCountryPixels(file, window, xPixel, yPixel)
-        news = getUncoloredPixelsNeighboursFromSameCountry(window, xPixel, yPixel)
-        for xPixel, yPixel in totalList:
-            c = window.get_at((xPixel, yPixel))
-            color = (c[0], c[1], c[2])
-            if color == yellow:
-                totalList.remove((xPixel, yPixel))
-        totalNews = list(news)
-        totalList += totalNews
-        total = tuple(totalList)
-    print("Finish")
-    file.close()
 
 
 def initColors():
@@ -364,7 +138,7 @@ if __name__ == '__main__':
     white = (0, 0, 0)  # blue
     green = (23, 165, 23)
     green1 = (0, 255, 0)
-    blue1 = (0, 0, 128)
+    blue1 = (0, 51, 153)
     pygame.init()
     window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     window.fill((255, 255, 255))
@@ -385,14 +159,13 @@ if __name__ == '__main__':
     while runing:
         ev = pygame.event.get()
         for event in ev:
-            if event.type == pygame.MOUSEBUTTONUP:
+            if event.type == pygame.MOUSEMOTION:
                 pos = pygame.mouse.get_pos()
+                print([pos[0], pos[1]])
                 # window.blit(text, (1020, 500))
-                # c = window.get_at((pos[0], pos[1]))
-                # print((c[0], c[1], c[2]))
-                # writeCountryPixelsInFile2("Romania", pos[0], pos[1])
-                # file = open("countries\\Romania\\pixels.txt", "w")
-                # findMoreCountryPixels(file, window, pos[0], pos[1])
-                drawCountry("Rusia", yellow)
+                # writeCountryPixelsInFile("Test", pos[0], pos[1])
+                drawCountry(pos[0], pos[1], blue1, yellow)
+                # initTreePixels()
+
         pygame.display.update()
     pygame.quit()
