f85be5bc106f0b898d85ff59a3e459f0abc53163
stoleriudaniel <stoleriu.daniel47@gmail.com>
2023-03-28 19:09:54 +0300

    Test color country on click v35

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ class Server():
         self.maximumPlayers = 2
         self.currentId = "0"
         self.state = ["0:(0,0);(click=0)", "1:(0,0);(click=0)"]
+        self.test = 0
 
     def create(self):
         try:
@@ -38,17 +39,17 @@ class Server():
                     conn.send(str.encode("Goodbye"))
                     break
                 else:
-                    print("Recieved: " + reply)
                     arr = reply.split(":")
                     id = int(arr[0])
                     self.state[id] = reply
+                    if ((self.state[0]).find("click=1") or (self.state[1]).find("click=1")) or self.test <= 9:
+                        print("test: ", self.test, " state:", self.state)
+                        self.test = self.test + 1
                     opponentId = 0
                     if id == 0: opponentId = 1
                     if id == 1: opponentId = 0
 
                     reply = self.state[opponentId][:]
-                    print("Sending: " + reply)
-
                 conn.sendall(str.encode(reply))
             except:
                 break
