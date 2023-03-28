6ec42ee9e4b34ebe8a7f4c6f1d54da6439aecbde
stoleriudaniel <stoleriu.daniel47@gmail.com>
2023-03-28 18:41:25 +0300

    Test color country on click v32

--- client.py
+++ client.py
@@ -105,7 +105,7 @@ class Client():
 
             # initTreePixels()
             # writeCountryPixelsInFile("Test", pos[0], pos[1])
-            if self.playerId == 0:
+            if self.playerId == "0":
                 self.game.player1.click = 0
             if self.game.player0.id == self.playerId:
                 data = f"{self.game.player0.id}:({str(self.game.player0.x)},{str(self.game.player0.y)});(click={self.game.player0.click})"
